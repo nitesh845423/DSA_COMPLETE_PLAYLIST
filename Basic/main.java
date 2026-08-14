6184d92252cf893cc6870a7f2e6ae03abc5d2675
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 00:17:15 +0530

    GitNote: 2026-08-14T18:47:15.130Z

--- Basic/main.java
+++ Basic/main.java
@@ -1,6 +1,7 @@
 public class main{
     public static void main(String[] args) {
-        System.out.print("Om Namha shivay");
+        System.out.print("Om Namha shivai");
         System.out.println("Hello World");
+        
     }
 }
