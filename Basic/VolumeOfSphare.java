7444a9325dd0ccc876daf4b85ed40bf5e1b07244
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 22:42:26 +0530

    GitNote: 2026-08-15T17:12:26.046Z

--- Basic/VolumeOfSphare.java
+++ Basic/VolumeOfSphare.java
@@ -7,6 +7,6 @@ public class VolumeOfSphare {
         double pi=22/7;
         Double Area=4*pi*r*r*r;
         Double Volume=4/3*pi*r*r*r;
-        System.out.println("Area and Volume of Sphare Respectively: "+Area+Volume);
+        System.out.println("Area and Volume of Sphare Respectively: "+Area+" "+Volume);
     }
 }
