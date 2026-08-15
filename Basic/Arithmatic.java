a9ada2e3c6906137ba3cfb639e8b89c65dcf4738
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 21:36:52 +0530

    GitNote: 2026-08-15T16:06:51.893Z

--- Basic/Arithmatic.java
+++ Basic/Arithmatic.java
@@ -1,6 +1,8 @@
 public class Arithmatic {
     public static void main(String[] args) {
         int x=39,y=10;
+        //integer ko operation karne par integer result milege
+        //like 3.9 ka 3 milega not 4 as roundoff
         System.out.println(x+y);
         System.out.println(x-y);
         System.out.println(x*y);
