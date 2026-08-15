9c0d256c0078dc5f89cabd6603d9a7dc81e6d992
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 16:09:07 +0530

    GitNote: 2026-08-15T10:39:07.526Z

--- Basic/printingnumbers.java
+++ Basic/printingnumbers.java
@@ -1,6 +1,6 @@
 public class printingnumbers {
     public static void main(String[] args) {
     System.out.println(69+1);
-    System.out.println("69+1");//
+    System.out.println("69+1");//here there is no mathmatical meaning of 69+1
     }
 }
