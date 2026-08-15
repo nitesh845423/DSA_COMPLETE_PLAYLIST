3ab3691e7f01d4c2c1eaace110ab7ee1105dd6b7
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 16:11:44 +0530

    GitNote: 2026-08-15T10:41:43.942Z

--- Basic/Variable.java
+++ Basic/Variable.java
@@ -1,5 +1,9 @@
 public class Variable {
     public static void main(String[] args) {
+        int x=5;
+        System.out.println(x);
+        System.out.println(x+7);
+        int y;
         
     }
 }
