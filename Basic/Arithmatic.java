18f1fa6986431361792f167d6fcdbe0bd1c96841
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 21:41:37 +0530

    GitNote: 2026-08-15T16:11:37.360Z

--- Basic/Arithmatic.java
+++ Basic/Arithmatic.java
@@ -3,13 +3,15 @@ public class Arithmatic {
         int x=39,y=10;
         //integer ko operation karne par integer result milege
         //like 3.9 ka 3 milega not 4 as round off
+        System.out.println("Integer result: ");
         System.out.println(x+y);
         System.out.println(x-y);
         System.out.println(x*y);
         System.out.println(x/y);
         double x1=39;
         double y1=10;
-        System.out.println(""+x+y);
+        System.out.println("Decimal result: ");
+        System.out.println(x+y);
         System.out.println(x-y);
         System.out.println(x*y);
         System.out.println(x1/y1);
