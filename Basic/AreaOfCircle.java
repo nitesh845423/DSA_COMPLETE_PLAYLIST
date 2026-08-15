10e0d4a30a2fae0f8f0d3987f984a4fcba94a48d
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 21:46:30 +0530

    GitNote: 2026-08-15T16:16:30.323Z

--- Basic/AreaOfCircle.java
+++ Basic/AreaOfCircle.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter radius of circle: ");
+        System.out.print("Enter radius of circle: ");
         //area of circel pi*r*r
         double pi=22/7;
         int r=sc.nextInt();
