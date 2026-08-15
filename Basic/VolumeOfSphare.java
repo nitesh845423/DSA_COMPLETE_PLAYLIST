7a29f5092c8f986f59232d7c6edb8fdf4dbe6336
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 22:38:08 +0530

    GitNote: 2026-08-15T17:08:08.546Z

--- Basic/VolumeOfSphare.java
+++ Basic/VolumeOfSphare.java
@@ -3,6 +3,8 @@ public class VolumeOfSphare {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter radius of sphere: ");
-        
+        int r=sc.nextInt();
+        double pi=22/7;
+
     }
 }
