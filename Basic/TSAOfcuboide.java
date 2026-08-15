da0484c00d5842ddd0b9d915e0da301917039cdc
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 01:24:07 +0530

    GitNote: 2026-08-15T19:54:07.078Z

--- Basic/TSAOfcuboide.java
+++ Basic/TSAOfcuboide.java
@@ -3,6 +3,7 @@ public class TSAOfcuboide {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter lenth of cuboid: ");
+        int l=sc.nextInt();
         System.out.println("Enter breath of cuboide: ");
         System.out.println("Enter height of cuboide: ");
     }
