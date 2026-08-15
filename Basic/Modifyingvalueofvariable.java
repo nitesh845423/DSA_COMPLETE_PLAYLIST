f192a80f4cca277848a63d38b3773d3bff83031d
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 21:27:18 +0530

    GitNote: 2026-08-15T15:57:18.055Z

--- Basic/Modifyingvalueofvariable.java
+++ Basic/Modifyingvalueofvariable.java
@@ -2,7 +2,11 @@ public class Modifyingvalueofvariable {
     public static void main(String[] args) {
         int x=7;
         System.out.println(x);
-        x+=2;
+        x=x+2;
+        System.out.println(x);
+        x=x-2;
+        System.out.println(x);
+        x=x*2;
         System.out.println(x);
     }
 }
