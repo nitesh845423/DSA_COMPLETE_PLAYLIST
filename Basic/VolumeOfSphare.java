a904e2422f914edb0c802e3407c08e72435fcff7
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 22:43:57 +0530

    GitNote: 2026-08-15T17:13:57.579Z

--- Basic/VolumeOfSphare.java
+++ Basic/VolumeOfSphare.java
@@ -5,7 +5,7 @@ public class VolumeOfSphare {
         System.out.print("Enter radius of sphere: ");
         int r=sc.nextInt();
         double pi=22/7;
-        Double Area=4*pi*r*r*r;
+        Double Area=4*pi*r*r;
         Double Volume=4/3*pi*r*r*r;
         System.out.println("Area and Volume of Sphare Respectively: "+Area+" "+Volume);
     }
