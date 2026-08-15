fc54995343ced2af7351f5ac5f04977a0deaad6d
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 21:31:26 +0530

    GitNote: 2026-08-15T16:01:26.635Z

--- Basic/Arithmatic.java
+++ Basic/Arithmatic.java
@@ -1,5 +1,9 @@
 public class Arithmatic {
     public static void main(String[] args) {
-        
+        int x=39,y=10;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
     }
 }
