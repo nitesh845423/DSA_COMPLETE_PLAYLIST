0c4a86bb5f660da0fcd34b3f195f5db14368e13b
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-15 22:48:09 +0530

    GitNote: 2026-08-15T17:18:08.887Z

--- Basic/VolumeOfSphare.java
+++ Basic/VolumeOfSphare.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
+
 public class VolumeOfSphare {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter radius of sphere: ");
-        int r=sc.nextInt();
-        double pi=22/7;
-        Double Area=4*pi*r*r;
-        Double Volume=4/3*pi*r*r*r;
-        System.out.println("Area and Volume of Sphare Respectively: "+Area+" "+Volume);
+        int r = sc.nextInt();
+
+        double pi = 22.0 / 7.0;
+
+        double Area = 4 * pi * r * r;
+        double Volume = (4.0 / 3.0) * pi * r * r * r;
+
+        System.out.println("Area and Volume of Sphere Respectively: "
+                + Area + " " + Volume);
     }
 }
