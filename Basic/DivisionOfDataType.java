32761e4042bd376d15bc3d8d1f8c91cc63140408
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 16:33:10 +0530

    GitNote: 2026-08-16T11:02:58.597Z

--- Basic/DivisionOfDataType.java
+++ Basic/DivisionOfDataType.java
@@ -1,7 +1,8 @@
 public class DivisionOfDataType {
     public static void main(String[] args) {
         double x = 5.0 / 2;
-        //int/int
+        //int/int->int
+        //double/int->
         System.out.println(x); // 2.5
     }
 }
