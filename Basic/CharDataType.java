74bbcf1a5f20f4cb996c10fbf168006650be4c79
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 17:26:46 +0530

    GitNote: 2026-08-16T11:56:45.712Z

--- Basic/CharDataType.java
+++ Basic/CharDataType.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 public class CharDataType {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        char a='a';
+        char c='a';
     }
 }
