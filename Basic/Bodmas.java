faf915579cfa8df49460f3a572a895593b782084
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-17 00:22:41 +0530

    GitNote: 2026-08-16T18:52:41.251Z

--- Basic/Bodmas.java
+++ Basic/Bodmas.java
@@ -3,6 +3,9 @@ public class Bodmas {
     public static void main(String[] args) {
         int a=10;
         System.out.println(a);
-
+        a=a+6;
+        System.out.println(a);
+        a=a*4;
+        System.out.println(a);
     }
 }
