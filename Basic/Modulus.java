c717ff1e631f586d6dce6cf8eaf7b323ca82677e
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 15:16:37 +0530

    GitNote: 2026-08-16T09:46:37.322Z

--- Basic/Modulus.java
+++ Basic/Modulus.java
@@ -4,6 +4,7 @@ public class Modulus {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter any number to get its modulus: ");
         int n=sc.nextInt();
-        
+        System.out.println("Enter second number ");
+
     }
 }
