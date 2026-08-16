6f78319159c2085694e55ce1b3cc6718fd348ad0
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 21:58:15 +0530

    GitNote: 2026-08-16T16:28:15.158Z

--- Basic/Ascii.java
+++ Basic/Ascii.java
@@ -3,6 +3,8 @@ public class Ascii {
         // Capital letters: A = 65 to Z = 90
         // Small letters:   a = 97 to z = 122
        // Digits:          0 = 48 to 9 = 57
-       //typecasting->ek data type se dusre data
+       //typecasting->ek data type se dusre data type me conversion
+       int x=10;
+       
     }
 }
