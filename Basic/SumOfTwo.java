acb7e69cf5e2057946c9e1fc83203c0d4420f0e5
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 13:40:12 +0530

    GitNote: 2026-08-16T08:10:12.572Z

--- Basic/SumOfTwo.java
+++ Basic/SumOfTwo.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class SumOfTwo {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
     }
 }
