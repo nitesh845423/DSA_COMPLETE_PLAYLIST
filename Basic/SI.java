3aea83625a0c1227a354e9f0532a200616a9dd40
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 13:50:58 +0530

    GitNote: 2026-08-16T08:20:58.483Z

--- Basic/SI.java
+++ Basic/SI.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class SI{
 public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    System.out.println("Enter number of");
+    System.out.println("Enter number Principal: ");
+    
 }
 }
