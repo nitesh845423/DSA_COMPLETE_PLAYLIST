7aa7fb45a5de2d3ef5601e2a228f975ec465cec1
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 13:37:50 +0530

    GitNote: 2026-08-16T08:07:50.180Z

--- Basic/squareofnum.java
+++ Basic/squareofnum.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class squareofnum {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter number: ");
-        
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        double Ans=n*n;
     }
 }
