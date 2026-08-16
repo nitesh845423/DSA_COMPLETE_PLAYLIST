52b1d7571b7af3a247386789158b933d92404932
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 13:51:22 +0530

    GitNote: 2026-08-16T08:21:22.608Z

--- Basic/SI.java
+++ Basic/SI.java
@@ -3,6 +3,8 @@ public class SI{
 public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter number Principal: ");
+    double p=sc.nextDouble();
+    System.out.println("Enter rae of interest: ");
     
 }
 }
