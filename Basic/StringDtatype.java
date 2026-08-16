24e83646d59055f3a59724055129643eb15485f5
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 17:30:03 +0530

    GitNote: 2026-08-16T12:00:03.247Z

--- Basic/StringDtatype.java
+++ Basic/StringDtatype.java
@@ -1,5 +1,7 @@
 public class StringDtatype {
     public static void main(String[] args) {
-        
+        String s1="Rahul";
+        System.out.println(s1);
+        System.out.println();
     }
 }
