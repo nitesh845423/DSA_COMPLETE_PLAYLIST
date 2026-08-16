e75fe01ecd7a22600eea808cb6b6ec544a8ecdb4
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-16 15:28:21 +0530

    GitNote: 2026-08-16T09:58:21.401Z

--- Basic/Modulus.java
+++ Basic/Modulus.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Modulus {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter any number: ");
+        System.out.print("Enter any number: ");
         int n=sc.nextInt();
-        System.out.println("Enter second number: ");
-
+        System.out.print("Enter second number: ");
+        int m=sc.nextInt();
+        int modulus=n%m;
+        System.out.print("Modulus of n and m is: " + modulus);
     }
 }
