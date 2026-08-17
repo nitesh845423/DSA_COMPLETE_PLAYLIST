5d4cfd80ec0992d1192c4fcd403b5492ad3640df
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-17 17:25:51 +0530

    GitNote: 2026-08-17T11:55:51.543Z

--- Basic/RelationalOpr.java
+++ Basic/RelationalOpr.java
@@ -1,6 +1,7 @@
 public class RelationalOpr {
     public static void main(String[] args) {
         System.out.println(5>3);
-        System.out.println();
+        System.out.println(8<=9);
+        System.out.println(5==4);
     }
 }
