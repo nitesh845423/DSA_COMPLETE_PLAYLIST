a625e6f80a6365615b643d72c5ab10a6c97a9427
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-17 17:30:02 +0530

    GitNote: 2026-08-17T12:00:02.214Z

--- Basic/RelationalOpr.java
+++ Basic/RelationalOpr.java
@@ -1,4 +1,4 @@
-public class RelationalOpr {//realtiona operators are used to compare two 
+public class RelationalOpr {//relational operators are used to compare two quantities
     public static void main(String[] args) {
         System.out.println(5>3);
         System.out.println(8<=9);
