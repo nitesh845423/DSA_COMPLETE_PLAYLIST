65519b36b53c84572df7e6f22f1dace5d37cf0a5
Nitesh Kumar Pandey <rauckeypandey8757860458@gmail.com>
2026-08-17 17:11:38 +0530

    GitNote: 2026-08-17T11:41:38.402Z

--- Basic/Bodmas.java
+++ Basic/Bodmas.java
@@ -8,7 +8,10 @@ public class Bodmas {
         a=a*4;
         System.out.println(a);
         int x=10;
-        System.out.println(++x);//preincrement->pahle ek badao o
+        System.out.println("preincrement: "+ ++x);//preincrement->pahle ek badao original value me then print karo ; so result will be 11
+        System.out.println("post increement: "+ x++);//post increment ->pahle value likho then increment karo//11 print hoga
+        System.out.println("Predecrement: "+ --x);//pre decrement ->pahle ghatao fir likho->11 print hoga
+        System.out.println("Post decrement: "+ x--);//post dcrement-> pahle likho fir ghatao->11
 
     }
 }
